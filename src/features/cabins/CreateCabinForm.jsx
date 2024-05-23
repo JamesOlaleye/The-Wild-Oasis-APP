f50eee9af4b617328fc8e134bd83359292306db9
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-05-23 11:28:42 +0100

    Uploading images to Supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -51,7 +51,6 @@ function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
   const { errors } = formState;
 
-
   const queryClient = useQueryClient();
 
   const { mutate, isLoading: isCreating } = useMutation({
@@ -65,7 +64,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -143,7 +142,13 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label='Cabin photo'>
-        <FileInput id='image' accept='image/*' />
+        <FileInput
+          id='image'
+          accept='image/*'
+          {...register('image', {
+            required: 'This field is required',
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zuvgplyzibypauqdhpqx.supabase.co';
+export const supabaseUrl = 'https://zuvgplyzibypauqdhpqx.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dmdwbHl6aWJ5cGF1cWRocHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0NDU5NzksImV4cCI6MjAzMTAyMTk3OX0.uRQM0u8vEjXxsDzmWnnoc64LpixtM3BE5J-N7CGPeaQ';
 const supabase = createClient(supabaseUrl, supabaseKey);
