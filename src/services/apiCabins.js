4c956b5aa5c11e142914094e4ff34b8c7d2ef6e2
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-05-28 07:53:53 +0100

    Duplicating Cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,7 +10,7 @@ export async function getCabins() {
 
   return data;
 }
- 
+
 export async function createEditCabin(newCabin, id) {
   const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
 
@@ -39,6 +39,8 @@ export async function createEditCabin(newCabin, id) {
   }
 
   // 2. Upload image
+  if (hasImagePath) return data;
+
   const { error: storageError } = await supabase.storage
     .from('cabin-images')
     .upload(imageName, newCabin.image);
